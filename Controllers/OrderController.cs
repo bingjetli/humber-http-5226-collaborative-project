00d71f8ec7b633bf47f300eb4965b00bd4c4d8e1
khushi patel <khuship6126@gmail.com>
2024-04-05 09:31:34 -0400

    cafecontroller

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using humber_http_5226_collaborative_project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace humber_http_5226_collaborative_project.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: Order
-        public ActionResult Index()
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static OrderController()
         {
-            return View();
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44328/api/");
+        }
+
+        // GET: Order/List
+        public ActionResult List(string SearchKey = null)
+        {
+            //objective: with our order data api to retrieve a list of orders
+            //curl https://localhost:44328/api/orderdata/listorders
+
+            string url = "orderdata/listorders/" + SearchKey;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<OrderDto> Orders = response.Content.ReadAsAsync<IEnumerable<OrderDto>>().Result;
+
+            //returns Views/Order/List.cshtml
+            return View(Orders);
         }
     }
 }
--- Controllers/OrderDataController.cs
+++ Controllers/OrderDataController.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using humber_http_5226_collaborative_project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace humber_http_5226_collaborative_project.Controllers
 {
     public class OrderDataController : ApiController
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        /// <summary>
+        /// Returns all orders in the system.
+        /// </summary>
+        /// <returns>
+        /// All orders in the database.
+        /// </returns>
+        /// <example>
+        /// GET: api/OrderData/ListOrders
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(OrderDto))]
+        public IHttpActionResult ListOrders()
+        {
+            List<Order> Orders = db.Orders.ToList();
+            List<OrderDto> OrderDtos = new List<OrderDto>();
+
+            Orders.ForEach(o => OrderDtos.Add(new OrderDto()
+            {
+                OrderId = o.OrderId,
+                CreatedAt = o.CreatedAt,
+                CafeId = o.CafeId,
+                CafeName = o.Cafe.Name,
+                CourierLicenseId = o.CourierLicenseId,
+                Status = o.Status
+            }));
+
+            return Ok(OrderDtos);
+        }
     }
 }
