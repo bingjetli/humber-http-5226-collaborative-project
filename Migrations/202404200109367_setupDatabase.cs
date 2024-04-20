a5c69c054b959ef69db51b712ac594fd34ab0748
Bailey Liang <bingjetli88@gmail.com>
2024-04-19 21:14:28 -0400

    fixed database issue

--- Migrations/202404200109367_setupDatabase.cs
+++ Migrations/202404200109367_setupDatabase.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class first : DbMigration
+    public partial class setupDatabase : DbMigration
     {
         public override void Up()
         {
